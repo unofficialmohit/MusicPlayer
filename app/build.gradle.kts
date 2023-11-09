db6d497f669416d9e0f9e3cd5c1193daebdc2ff0
mohit <mohitgujjar2121@gmail.com>
2023-11-09 19:09:28 +0530

    just version number 1.7.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "1.7.0"
+        versionName = "1.7.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
